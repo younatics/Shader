40e1cbb735cdf0091cc84b8ae2dd61e29b186258
Seungyoun Yi <younatics@gmail.com>
2017-09-05 14:37:59 +0900

    add init

---
+++ Shader/Shader.h
@@ -0,0 +1,19 @@
+//
+//  Shader.h
+//  Shader
+//
+//  Created by Seungyoun Yi on 2017. 9. 5..
+//  Copyright © 2017년 Seungyoun Yi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Shader.
+FOUNDATION_EXPORT double ShaderVersionNumber;
+
+//! Project version string for Shader.
+FOUNDATION_EXPORT const unsigned char ShaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shader/PublicHeader.h>
+
+
