29c136a0472343b9df57dbae81ab707eed5319d6
Seungyoun Yi <younatics@gmail.com>
2017-09-05 15:25:27 +0900

    add shader methods

--- Shader/Shader.swift
+++ Shader/Shader.swift
@@ -7,11 +7,67 @@
 //
 
 import UIKit
+import QuartzCore
 
 public class Shader {
-    public class func at(frame: CGRect) -> UIView {
+    public class func at(frame: CGRect, color: UIColor) -> UIView {
+        let shaderView = UIView(frame: UIScreen.main.bounds)
+        shaderView.backgroundColor = color
+        
+        let mutablePath = CGMutablePath()
+        mutablePath.addRect(shaderView.bounds)
+        mutablePath.addRect(frame)
+        
+        shaderView.layer.mask = shaderMask(path: mutablePath)
+        return shaderView
+    }
+    
+    public class func at(frame: CGRect, cornerRadius: CGFloat, color: UIColor) -> UIView {
+        let shaderView = UIView(frame: UIScreen.main.bounds)
+        shaderView.backgroundColor = color
+        
+        let bezierPath = UIBezierPath(rect: UIScreen.main.bounds)
+        let cornerPath = UIBezierPath(roundedRect: frame, cornerRadius: cornerRadius)
+        bezierPath.append(cornerPath)
         
         
-        return UIView()
+        shaderView.layer.mask = shaderMask(path: bezierPath.cgPath)
+        return shaderView
+    }
+    
+    public class func at(frames: [CGRect], color: UIColor) -> UIView {
+        let shaderView = UIView(frame: UIScreen.main.bounds)
+        shaderView.backgroundColor = color
+        
+        let mutablePath = CGMutablePath()
+        mutablePath.addRect(shaderView.bounds)
+        for frame in frames {
+            mutablePath.addRect(frame)
+        }
+        
+        shaderView.layer.mask = shaderMask(path: mutablePath)
+        return shaderView
+    }
+    
+    public class func at(framesAndRadius:[(frame: CGRect, cornerRadius: CGFloat)], color: UIColor) -> UIView {
+        let shaderView = UIView(frame: UIScreen.main.bounds)
+        shaderView.backgroundColor = color
+        
+        let bezierPath = UIBezierPath(rect: UIScreen.main.bounds)
+        for frameAndRadius in framesAndRadius {
+            let cornerPath = UIBezierPath(roundedRect: frameAndRadius.frame, cornerRadius: frameAndRadius.cornerRadius)
+            bezierPath.append(cornerPath)
+        }
+        
+        shaderView.layer.mask = shaderMask(path: bezierPath.cgPath)
+        return shaderView
+    }
+    
+    private class func shaderMask(path:CGPath) -> CAShapeLayer {
+        let mask = CAShapeLayer()
+        mask.path = path
+        mask.fillRule = kCAFillRuleEvenOdd
+
+        return mask
     }
 }
--- ShaderDemo/ViewController.swift
+++ ShaderDemo/ViewController.swift
@@ -11,11 +11,16 @@ import Shader
 
 class ViewController: UIViewController {
     @IBOutlet var originView: UIView!
+    @IBOutlet var originView2: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let shaderView = Shader.at(frame: originView.frame)
+        originView.layer.cornerRadius = 50
+        
+        let shaderView = Shader.at(frame: originView.frame, color: UIColor.blue.withAlphaComponent(0.3))
+//        let shaderView = Shader.at(frames: [originView.frame, originView2.frame], color: UIColor.black.withAlphaComponent(0.5))
+//        let shaderView = Shader.at(frame: originView.frame, cornerRadius: 50, color: UIColor.black.withAlphaComponent(0.5))
         self.view.addSubview(shaderView)
         
     }
